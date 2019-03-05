cc28b0e5763c9768f72e470ae1c5533d359e7505
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-03-05 12:28:12 +0100

    aggiunto componente error e home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,13 +5,15 @@ import { Routes, RouterModule} from '@angular/router';
 //Import Components
 import  { LoginComponent } from './components/login/login.component';
 import  { RegisterComponent } from './components/register/register.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 //Definire le route
 const appRoutes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'inicio', component: LoginComponent},
+  {path: '', component: HomeComponent},
+  {path: 'inicio', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'registro', component: RegisterComponent}
+  {path: 'registro', component: RegisterComponent},
+  {path: '**', component: ErrorComponent}
 ];
 
 //Esportare la configurazione
